7120b8ce42bd5f46866263911e5abc4dc828bf74
Laurent HUBERT <lau.hub@gmail.com>
2017-06-30 12:40:26 +0200

    Ajout: base de données de test (4)

--- exemples/demojava.sql
+++ exemples/demojava.sql
@@ -1,5 +1,6 @@
 create database demojava ;
 CREATE USER 'demouser'@'%' IDENTIFIED BY 'MotDePasse';
 GRANT ALL PRIVILEGES ON demojava.* TO "demouser"@"%" IDENTIFIED BY "MotDePasse";
+USE demojava;
 create table personne (nom varchar(64), prenom varchar(64), dnaissance date);
 insert into personne values ('Luther King','Martin','1929-01-15');
